ddaacef6f101d00ef18e6a3db2b33ecfffeb0a8e
gabrown21 <gabrown@bu.edu>
2024-09-08 14:09:37 -0400

    Finished Excercise 2 with the Mii theme song

--- assignment/Report.md
+++ assignment/Report.md
@@ -1,3 +1,6 @@
 Excercise 1:
 Min_Bright: 43,000
 Max_Bright: 1,300
+
+Excercise 2: Wii Mii Theme Song 
+
--- assignment/exercise_sound.py
+++ assignment/exercise_sound.py
@@ -25,15 +25,49 @@ def quiet():
     speaker.duty_u16(0)
 
 
-freq: float = 30
-duration: float = 0.1  # seconds
+DS5 = 622
+GS5 = 830
+FS5 = 739
+E5 = 659
+B4 = 494
+A4 = 440
+FS4 = 370
+CS5 = 554
+D4 = 294
+CS4 = 277
+F4 = 349
+D5 = 587
+GS4 = 415
+G4 = 392
+
+b_d = 60 / 114
+DURATIONS = {
+    '1': b_d * 4,    
+    '2': b_d * 2,    
+    '-4': b_d * 1.5, 
+    '4': b_d * 1,    
+    '8': b_d * 0.5  
+}
+
+melody = [
+    (FS4, DURATIONS['8']), (A4, DURATIONS['8']), (CS5, DURATIONS['8']),
+    (A4, DURATIONS['8']), (FS4, DURATIONS['8']), (D4, DURATIONS['8']),
+    (D4, DURATIONS['8']), (D4, DURATIONS['8']),
+    (CS4, DURATIONS['8']), (D4, DURATIONS['8']), (FS4, DURATIONS['8']), 
+    (A4, DURATIONS['8']), (CS5, DURATIONS['8']), (A4, DURATIONS['8']),
+    (F4, DURATIONS['8']), (E5, DURATIONS['-4']), (DS5, DURATIONS['8']), (D5, DURATIONS['8']),
+    (GS4, DURATIONS['8']), (CS5, DURATIONS['8']), (FS4, DURATIONS['8']),
+    (CS5, DURATIONS['8']), (GS4, DURATIONS['8']), (CS5, DURATIONS['8']),
+    (G4, DURATIONS['8']), (FS4, DURATIONS['8']),              
+]
 
 print("Playing frequency (Hz):")
 
-for i in range(64):
+#(wii theme song)
+for freq, duration in melody:
     print(freq)
     playtone(freq, duration)
-    freq = int(freq * 1.1)
-
+    quiet()
+    utime.sleep(0.05) 
 # Turn off the PWM
 quiet()
